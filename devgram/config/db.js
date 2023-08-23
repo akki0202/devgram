28e781932347bd27a67aacfb9d2098ab46a642d3
akki0202 <you@example.com>
2023-08-23 22:26:24 +0530

    dotenv

--- devgram/config/db.js
+++ devgram/config/db.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = config.get('mongoURI');
-
+// const dotenv = require("dotenv");
+// const db = config.get('mongoURI');
+// console.log(db);
 const connectDB = async () => {
     try {
-        await mongoose.connect(db);
+        await mongoose.connect(process.env.mongoURI);
 
         console.log("MongoDB connected..");
     }
